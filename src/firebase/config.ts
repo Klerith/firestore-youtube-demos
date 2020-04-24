b67e060635d26bf3321dd51c81d134384698a47d
Fernando Herrera <fernando.herrera85@gmail.com>
2020-04-24 16:41:52 -0400

    cadena de conexion guardada

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-throw new Error('Configuren su cadena de conexión a Firebase');
-
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDW_fP9ieYCCBRtm62kRJsnUa_IvGCu2S4",
+    authDomain: "sql-demos.firebaseapp.com",
+    databaseURL: "https://sql-demos.firebaseio.com",
+    projectId: "sql-demos",
+    storageBucket: "sql-demos.appspot.com",
+    messagingSenderId: "460621037254",
+    appId: "1:460621037254:web:676a89808ba64b96e221c7",
+    measurementId: "G-MKJBQWHQEP"
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
